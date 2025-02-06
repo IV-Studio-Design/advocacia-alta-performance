27a5763183dc47949cfb6e7eb04bec76d63790bb
= <thales.mariano125@gmail.com>
2025-02-06 16:54:02 -0300

    fix: container centralized

--- components/DescubraSection.vue
+++ components/DescubraSection.vue
@@ -3,9 +3,9 @@
         <img class="w-[50rem] absolute -top-[35rem] -left-52 mix-blend-screen opacity-50 z-40" src="@/assets/images/light_effect.png" />
         <img class="w-[50rem] absolute -bottom-[30rem] -right-56 mix-blend-screen opacity-50 z-40" src="@/assets/images/light_effect.png" />
 
-        <div class="w-full h-full flex gap-3 justify-start items-center bg-[url('@/assets/images/building_bg.png')] bg-cover bg-center">
-            <div class="w-[45%] h-full relative self-end">
-                <img class="w-[40rem] absolute bottom-20 -right-36 mix-blend-screen z-40" src="@/assets/images/light_effect.png" />
+        <div class="w-full h-full flex gap-3 justify-center items-center bg-[url('@/assets/images/building_bg.png')] bg-cover bg-center">
+            <div class="w-96 h-full relative self-end">
+                <img class="w-[40rem] absolute bottom-44 -right-10 mix-blend-screen z-40" src="@/assets/images/light_effect.png" />
                 <img class="w-[25rem] absolute bottom-0 right-0 z-50" src="@/assets/images/statue.png">
             </div>
             <div class="space-y-10 py-20 z-50">
